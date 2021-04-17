12f6d145cc32817abf46804f29dbf5ed3c1cdc65
Eiji MATSUMOTO <e.mattsan@gmail.com>
2021-04-17 14:42:30 +0900

    initial commit

---
+++ lib/pngex.ex
@@ -0,0 +1,18 @@
+defmodule Pngex do
+  @moduledoc """
+  Documentation for `Pngex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pngex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pngex_test.exs
@@ -0,0 +1,8 @@
+defmodule PngexTest do
+  use ExUnit.Case
+  doctest Pngex
+
+  test "greets the world" do
+    assert Pngex.hello() == :world
+  end
+end
