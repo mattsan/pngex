a40166730790354c37c4abfeb7a202d7b6c44533
Eiji MATSUMOTO <e.mattsan@gmail.com>
2023-02-12 19:28:54 +0900

    clean test helper up

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,91 +4,81 @@ defmodule TestPixel do
   def get_rgb(%{width: width, height: height, depth: 8}, n) do
     r = div(n, width)
     g = rem(n, height)
-    b = 255 - max(r, g)
+    b = 0xFF - max(r, g)
     {r, g, b}
   end
 
   def get_rgb(%{width: width, height: height, depth: 16}, n) do
-    r = div(n, width) * 256
-    g = rem(n, height) * 256
-    b = 65535 - max(r, g)
+    r = div(n, width) <<< 8
+    g = rem(n, height) <<< 8
+    b = 0xFFFF - max(r, g)
     {r, g, b}
   end
 
   def get_grayscale(%{width: width, height: height, depth: 16}, n) do
-    rem(rem(n, width) * 256 + 255 + div(n, height) * 256 + 255, 1 <<< 16)
+    rem((rem(n, width) <<< 8) + 0xFF + (div(n, height) <<< 8) + 0xFF, 1 <<< 16)
   end
 
-  def get_grayscale(%{width: width, height: height, depth: depth}, n) do
+  def get_grayscale(%{width: width, height: height, depth: depth}, n)
+      when depth in [1, 2, 4, 8] do
     rem(rem(n, width) + div(n, height), 1 <<< depth)
   end
 
   def get_palette(%{depth: 8}) do
-    Enum.map(0..255, fn n ->
-      r = div(n, 16) * 16 + 15
-      g = rem(n, 16) * 16 + 15
-      b = 255 - max(r, g)
+    Enum.map(0..0xFF, fn n ->
+      r = div(n, 0x10) <<< (4 + 0x0F)
+      g = rem(n, 0x10) <<< (4 + 0x0F)
+      b = 0xFF - max(r, g)
       {r, g, b}
     end)
   end
 
   def get_palette(%{depth: 4}) do
-    Enum.map(0..15, fn n ->
-      r = div(n, 4) * 64 + 63
-      g = rem(n, 4) * 64 + 63
-      b = 255 - max(r, g)
+    Enum.map(0..0x0F, fn n ->
+      r = div(n, 0x04) <<< (6 + 0x3F)
+      g = rem(n, 0x04) <<< (6 + 0x3F)
+      b = 0xFF - max(r, g)
       {r, g, b}
     end)
   end
 
   def get_palette(%{depth: 2}) do
     [
-      {0, 0, 255},
-      {0, 255, 0},
-      {255, 0, 0},
-      {255, 255, 0}
+      {0x00, 0x00, 0xFF},
+      {0x00, 0xFF, 0x00},
+      {0xFF, 0x00, 0x00},
+      {0xFF, 0xFF, 0x00}
     ]
   end
 
   def get_palette(%{depth: 1}) do
     [
-      {0, 0, 255},
-      {255, 255, 0}
+      {0x00, 0x00, 0xFF},
+      {0xFF, 0xFF, 0x00}
     ]
   end
 
   def get_color_index(%{depth: 8}, x, y) do
-    div(y, 16) * 16 + div(x, 16)
+    div(y, 0x10) <<< (16 + div(x, 0x10))
   end
 
   def get_color_index(%{depth: 4}, x, y) do
-    div(y, 64) * 4 + div(x, 64)
+    div(y, 0x40) <<< (2 + div(x, 0x40))
   end
 
   def get_color_index(%{depth: 2}, x, y) do
-    div(y, 128) * 2 + div(x, 128)
+    div(y, 0x80) <<< (1 + div(x, 0x80))
   end
 
   def get_color_index(%{depth: 1}, x, y) do
-    if x + y < 256 do
+    if x + y < 0x100 do
       0
     else
       1
     end
   end
 
-  def get_alpha(%{width: width, height: height, depth: 16}, n) do
-    x = rem(n, width) / width
-    y = div(n, width) / height
-
-    if 0.25 <= x && x < 0.75 && 0.25 <= y && y < 0.75 do
-      (1 <<< 16) - 1
-    else
-      0
-    end
-  end
-
-  def get_alpha(%{width: width, height: height, depth: depth}, n) do
+  def get_alpha(%{width: width, height: height, depth: depth}, n) when depth in [8, 16] do
     x = rem(n, width) / width
     y = div(n, width) / height
 
